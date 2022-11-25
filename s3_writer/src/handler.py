21c9de64b35c975c589a41009c85689c99748ad2
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2022-11-25 08:43:26 +0100

    handler returns string from non standard library

--- s3_writer/src/handler.py
+++ s3_writer/src/handler.py
@@ -1,18 +1,6 @@
-import os
-import uuid
-
 from faker import Faker
-
-from s3_driver import S3Driver
 
 
 def handle_request(event, context) -> str:
-    content = Faker().name()
-
-    s3_driver = S3Driver.default(bucket_name())
-    s3_driver.write_to_file(content, str(uuid.uuid4()))
-    return "OK"
-
-
-def bucket_name():
-    return str(os.environ.get("S3_BUCKET"))
+    random_string = Faker.pystr(10)
+    return random_string
